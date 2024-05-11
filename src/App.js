355fe70bc68f1c361c7fb32477bd06952793c2a0
John Doe <get.in.touch.with.me@skiff.com>
2024-05-11 10:08:02 -0700

    react heart candy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
+import Header from './components/Header';
+//import Heart from './components/Heart';
+import HeartsList from './components/HeartsList';
 import './App.css';
+const message2 = 'I ❤️ React'
+//const message = 'Candy';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <>
+      <Header msg2={message2} />
+      <HeartsList />
+      {/*<Heart msg={message} />*/}
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+
+const Header = (props) => {
+    return (
+      <div className="App-header">
+           {props.msg2}
+      </div>
+    )
+  } 
+  export default Header;
